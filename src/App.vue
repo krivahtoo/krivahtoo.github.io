a442df3ec9c61650ce9fe416101444c2d9c02bab
Noah Too <krivahtoo@gmail.com>
2021-03-10 21:26:44 +0300

    Initial layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TheHeader />
+    <div class="circle1"></div>
+    <div class="circle2"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TheHeader from './components/TheHeader'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
-  }
-};
+    TheHeader,
+  },
+}
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #41388f;
+  font-family: 'Poppins', sans-serif;
+  min-height: 100vh;
+  background: linear-gradient(to right top, #28a991, #23a1b5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+h1 {
+  color: #092344;
+  font-weight: 600;
+  font-size: 3rem;
+  opacity: 0.8;
+}
+h2,
+p {
+  color: #183763;
+  font-weight: 500;
+  opacity: 0.8;
+}
+
+h3 {
+  color: #2a4364;
+  font-weight: 600;
+  opacity: 0.8;
+}
+.circle1,
+.circle2 {
+  background: white;
+  background: linear-gradient(
+    to right bottom,
+    rgba(255, 255, 255, 0.8),
+    rgba(255, 255, 255, 0.3)
+  );
+  height: 20rem;
+  width: 20rem;
+  position: absolute;
+  border-radius: 50%;
+}
+
+.circle1 {
+  top: 5%;
+  right: -5%;
+}
+.circle2 {
+  bottom: 5%;
+  left: -10%;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="header">
+    <div class="pic">
+      <img id="pic" alt="Vue logo" src="../assets/noah.png" />
+    </div>
+    <div id="tag">
+      <div id="name">
+        <h1>Noah Cheptoo</h1>
+      </div>
+      <div id="title">
+        <h2>Programmer | Full Stack Web Developer</h2>
+      </div>
+    </div>
+    <div id="info">
+      <h3>I am a Self tought programmer</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+}
+</script>
+
+<style lang="scss">
+#header {
+  background: white;
+  min-height: 90vh;
+  width: 25rem;
+  background: linear-gradient(
+    to right bottom,
+    rgba(255, 255, 255, 0.7),
+    rgba(255, 255, 255, 0.3)
+  );
+  border-radius: 2rem;
+  z-index: 2;
+  backdrop-filter: blur(2rem);
+  #pic {
+    height: 50%;
+    width: 50%;
+    margin-top: 3rem;
+    margin-bottom: 3rem;
+    border-radius: 40%;
+  }
+  #tag {
+    background: linear-gradient(to right top, #65dfc9, #6cdbeb);
+    border-radius: 2rem;
+    color: white;
+    padding: 1rem;
+    position: relative;
+  }
+  #info {
+    margin-top: 1.5rem;
+  }
+}
+</style>
